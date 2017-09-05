0c8297c0eb580f7a88f4140661f9e6451e668fd9
Hugo Kamps <hugokamps6@hotmail.com>
2017-09-05 12:35:20 +0200

    Character and movement

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Character {
+    public class Character : MonoBehaviour {
+        public static readonly string ColliderTag = "Ground";
+        private CharacterTranslation _translation;
+
+        public float Speed { get; set; }
+        public float JumpForce { get; set; }
+        public Camera camera;
+
+        private void Awake() {
+            _translation = new CharacterTranslation(this);
+            JumpForce = 20.0f;
+            Speed = 10.0f;
+            camera = GetComponentInChildren<Camera>();
+        }
+
+        private void FixedUpdate() {
+            _translation.Update();
+            camera.transform.position.Set(camera.transform.position.x, transform.position.y, 1);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision) {
+            if (collision.gameObject.tag == ColliderTag && _translation.Airborne) {
+                _translation.Airborne = false;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision) {
+            if (collision.gameObject.name == "end") {
+                Debug.Log("end");
+            }
+        }
+    }
+}
