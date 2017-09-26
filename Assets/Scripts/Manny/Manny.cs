f89c7a7baf86bb3af2fa2c03ab7d91a942e54dec
Hugo Kamps <hugokamps6@hotmail.com>
2017-09-26 13:19:56 +0200

    Manny core
    - Attribute system

---
+++ Assets/Scripts/Manny/Manny.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manny : MonoBehaviour {
+    private MannyBrain _mannyBrain;
+    private MannyAttribute _mannyAttribute;
+
+    // Use this for initialization
+    private void Start() {
+        _mannyBrain = new MannyBrain();
+        _mannyAttribute = new MannyAttribute();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        _mannyBrain.Update();
+    }
+
+    private void OnApplicationQuit() {
+        _mannyAttribute.Save();
+    }
+}
---
+++ Assets/Scripts/Manny/MannyBrain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MannyBrain {
+
+    private MannyAttribute _mannyAttribute;
+
+    public MannyBrain(MannyAttribute mannyAttribute) {
+        _mannyAttribute = mannyAttribute;
+    }
+	public void Update () {
+        // Deminish food and thirst
+    }
+}
