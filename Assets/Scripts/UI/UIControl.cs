55f2eb73150f9b9eef865b0b47dd9d5e2a4def91
dubehh <eelcoeikelboom@hotmail.com>
2017-09-26 16:01:44 +0200

    UIControl manager + dynamic loading

---
+++ Assets/Scripts/UI/UIControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.UI {
+    [Serializable]
+    public class UIControl {
+
+        [SerializeField]
+        public bool Default;
+        [SerializeField]
+        public GameObject Parent;
+        public bool Active { get; set; }
+
+        public void Toggle(bool active) {
+            Active = active;
+            Parent.SetActive(active);
+        }
+
+        public string GetName() {
+            return Parent.name;
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Scripts.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField]
+    public UIControl[] Controls;
+    private UIControl _current;
+
+    private void Awake() {
+        LoadDefault();
+        foreach (var ctrl in Controls)
+            if (!ctrl.Active && ctrl.Parent.activeSelf) {
+                ctrl.Toggle(false);
+            }
+    }
+
+    private void LoadDefault() {
+        var ctrl = Controls.Where(x => x.Default).FirstOrDefault();
+        if (ctrl != null) {
+            _current = ctrl;
+            ctrl.Toggle(true);
+        }
+    }
+
+    public void View(string control) {
+        var ctrl = Get(control);
+        if (ctrl.Equals(default(UIControl))) LoadDefault();
+        else View(ctrl);
+    }
+
+    public void View(UIControl control) {
+        if (_current != null)
+            _current.Toggle(false);
+        control.Toggle(true);
+        OnControllerChange(control);
+        _current = control;
+    }
+
+    private UIControl Get(string name){
+        return Controls.Where(ctrl => ctrl.GetName() == name).FirstOrDefault();
+    }
+ 
+	private void Update () {
+		
+	}
+
+    private void OnControllerChange(UIControl ctrl) {
+        //fired when the controller changed
+    }
+
+    public void OnClick() {
+        View("ShopControl");
+    }
+}
