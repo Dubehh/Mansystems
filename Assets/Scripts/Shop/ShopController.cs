85575b1cfa8158cab5fb9f9e8974f63b9e85bcc5
Hugo Kamps <hugokamps6@hotmail.com>
2017-10-04 15:37:51 +0200

    Scrollpanel fixed definitive

--- Assets/Scripts/Shop/ShopController.cs
+++ Assets/Scripts/Shop/ShopController.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 [Serializable]
 public class ShopController : MonoBehaviour {
 
-    private const float yStart = 315f;
+    private const float _yStart = 315f;
+    private const byte _transparencyStart = 255;
 
     [SerializeField]
     public ShopItem[] Items;
@@ -13,24 +14,34 @@ public class ShopController : MonoBehaviour {
     [SerializeField]
     public GameObject Item;
 
+    private RectTransform ScrollRect;
+
     // Use this for initialization
     void Start() {
-        var y = yStart;
-        byte transparency = 255;
+        ScrollRect = GetComponent<RectTransform>();
+
+        var y = _yStart;
+        var transparency = _transparencyStart;
         foreach (var item in Items) {
-            if (transparency < 140) transparency = 255;
+            if (transparency < 140) transparency = _transparencyStart;
 
             var obj = Instantiate(Item, transform).GetComponent<ShopItemPrefab>();
             obj.Item = item;
             obj.Init();
 
             obj.GetComponent<Image>().color = item.Attribute == Attribute.Food 
-                ? new Color32(18, 178, 112, transparency) 
+                ? new Color32(18, 178, 112, transparency)
                 : new Color32(26, 118, 175, transparency);
 
             obj.transform.localPosition = new Vector2(0, y);
             y -= obj.GetComponent<RectTransform>().rect.height - 2;
             transparency -= 30;
         }
+    }
+
+    public void OnValueChanged(Vector2 vec) {
+        var maxY = 540;
+        if (vec.y > 0) ScrollRect.anchoredPosition = new Vector2(0, 0);
+        if (ScrollRect.anchoredPosition.y > maxY) ScrollRect.anchoredPosition = new Vector2(0, maxY);
     }
 }
