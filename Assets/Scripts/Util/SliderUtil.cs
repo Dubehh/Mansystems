a44abf67f40d6f89dcb788a91a35578b9f18f651
Hugo Kamps <hugokamps6@hotmail.com>
2017-10-10 14:15:31 +0200

    Slider background color util

---
+++ Assets/Scripts/Util/SliderUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class SliderUtil {
+    public static Slider GetSliderBackground(Slider slider) {
+        var baseColor = new Color();
+        Image background = null;
+        foreach (var image in slider.GetComponentsInChildren<Image>()) {
+            if (image.name == "Fill") baseColor = image.color;
+            if (image.name == "Background") background = image;
+        }
+
+        baseColor.a /= 1.5f;
+        background.color = baseColor;
+
+        return slider;
+    }
+}
