2bdf64284a491b2b9fb367b6cbe753874fc300d3
Hugo Kamps <hugokamps6@hotmail.com>
2017-10-12 14:43:05 +0200

    Minigames implementation and shop layout fix

---
+++ Assets/Scripts/Minigames/MinigamePrefab.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class MinigamePrefab : MonoBehaviour {
+
+    public Minigame Minigame { get; set; }
+
+    [SerializeField]
+    public RawImage Icon;
+
+    [SerializeField]
+    public Text Title;
+
+    [SerializeField]
+    public Text Description;
+
+    /// <summary>
+    /// Fill the prefab with the information from the minigame
+    /// </summary>
+    public void Init() {
+        Icon.texture = Minigame.Icon;
+        Title.text = Minigame.Title;
+        Description.text = Minigame.Description;
+    }
+
+    /// <summary>
+    /// OnClick event for the minigame's buy button
+    /// </summary>
+    public void OnClick() {
+        Minigame.StartGame();
+    }
+}
---
+++ Assets/Scripts/Minigames/MinigamesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class MinigamesController : MonoBehaviour {
+
+    private float _prefabHeight;
+
+    [SerializeField]
+    public Minigame[] Minigames;
+
+    [SerializeField]
+    public GameObject Minigame;
+
+    // Use this for initialization
+    void Start() {
+        var y = -75f;
+
+        // Loop through the Minigame array and create a MinigamePrefab with each one of them
+        foreach (var game in Minigames) {
+            var obj = Instantiate(Minigame, transform).GetComponent<MinigamePrefab>();
+            obj.Minigame = game;
+            obj.Init();
+
+            var rect = obj.GetComponent<RectTransform>().rect;
+
+            _prefabHeight = rect.height;
+
+            obj.transform.localPosition = new Vector2(rect.width / 2, y);
+            y -= _prefabHeight;
+        }
+    }
+
+    /// <summary>
+    /// This method makes sure that the user cannot scroll outside of the content of the panel
+    /// </summary>
+    /// <param name="vec">The 2D vector of the current location of the scroll content</param>
+    public void OnValueChanged(Vector2 vec) {
+        var scrollView = transform.parent.parent;
+        var capacity = Math.Floor(scrollView.GetComponent<RectTransform>().rect.height / _prefabHeight);
+        var _scrollRect = GetComponent<RectTransform>();
+
+        if (capacity < Minigames.Length) {
+            var maxY = (float)((Minigames.Length - capacity) * _prefabHeight) - 25f;
+            if (_scrollRect.anchoredPosition.y < 0) _scrollRect.anchoredPosition = new Vector2();
+            if (_scrollRect.anchoredPosition.y > maxY) _scrollRect.anchoredPosition = new Vector2(0, maxY);
+        } else {
+            _scrollRect.anchoredPosition = new Vector2();
+        }
+    }
+}
