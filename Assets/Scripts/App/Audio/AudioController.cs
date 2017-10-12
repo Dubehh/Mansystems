bef7ce46e57129a2cc1c598c4bea61f7fa945c08
Danny <dannyvansanten@gmail.com>
2017-10-12 15:29:55 +0200

    audio fix

--- Assets/Scripts/App/Audio/AudioController.cs
+++ Assets/Scripts/App/Audio/AudioController.cs
@@ -15,6 +15,7 @@ namespace Assets.Scripts.App {
 
         private void Awake() {
             _source = GetComponent<AudioSource>();
+            Play("return");
             _items = new Dictionary<string, AudioClip>();
             foreach (var item in Items)
                 _items[item.Key.ToLower()] = item.Clip;
@@ -25,21 +26,11 @@ namespace Assets.Scripts.App {
         /// This name should be unique.
         /// </summary>
         /// <param name="name">The name of the clip</param>
-        public void Play(string name) {
-            if (_items.ContainsKey(name.ToLower())) {
+        public void Play(string name){
+            if (_items.ContainsKey(name.ToLower())){
                 _source.clip = _items[name.ToLower()];
                 _source.Play();
             }
-            name = "return"; //Plays when player returns to the dashboard
-            name = "fwd"; //Plays when player goes away from dashboard into other screen
-            name = "lvl"; //Plays when player levels up
-            name = "startup"; //Plays when game starts up
-            name = "coinsound"; //Plays when player picks up 
-            name = "button"; //Plays when player presses a button
-            name = "notif"; //Plays when notification appears ingame
-            name = "vsound"; //Plays when player wins the game, plays when popup for replay/quit appears
-            name = "lsound"; //Plays when player loses the game, plays when popup for replay/quit appears
-            name = "cjfs"; //Plays when player falls off off the platform
-            }
+        }
     }
 }
