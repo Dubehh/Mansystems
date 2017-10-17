7a8fd065fd0558e90290b5db3ecc5347eebff963
dubehh <eelcoeikelboom@hotmail.com>
2017-10-17 15:07:35 +0200

    Audio settings

---
+++ Assets/Scripts/App/Audio/AudioSettingController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSettingController : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite _soundEnabled;
+    [SerializeField]
+    private Sprite _soundDisabled;
+    private Image _image;
+    private bool _enabled;
+
+    private void Start() {
+        _enabled = true;
+        _image = GetComponentInChildren<Image>();
+    }
+
+    public void OnSettingChange() {
+        _enabled = !_enabled;
+        _image.sprite = _enabled ? _soundEnabled : _soundDisabled;
+        AudioListener.pause = _enabled;
+        AudioListener.volume = _enabled ? 1 : 0;
+    }
+}
