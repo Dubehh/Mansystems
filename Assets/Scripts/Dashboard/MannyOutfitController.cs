7d9104ce8fc12fc6bbc50bf3a670e9f6c95c6347
Hugo Kamps <hugokamps6@hotmail.com>
2017-11-02 12:59:25 +0100

    Manny different outfit prefabs

---
+++ Assets/Scripts/Dashboard/MannyOutfitController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MannyOutfitController : MonoBehaviour {
+
+    private const string _prefix = "Assets/Resources/Manny/";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
