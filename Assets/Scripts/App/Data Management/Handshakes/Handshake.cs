2e75cd165635e56ac2e1c6f6792e8dec862741e8
dubehh <eelcoeikelboom@hotmail.com>
2017-11-14 15:00:29 +0100

    handshake core

---
+++ Assets/Scripts/App/Data Management/Handshakes/Handshake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts.App.Data_Management {
+    public class Handshake {
+
+        private const string
+            _webReference = "http://localhost/",
+            _webController = "upload";
+
+        private const string _handshakeID = "requestType";
+        private List<IMultipartFormSection> _params;
+        private HandshakeProtocol _protocol;
+
+        public Handshake(HandshakeProtocol protocol) {
+            _protocol = protocol;
+            _params = new List<IMultipartFormSection>();
+            AddParameter(_handshakeID, _protocol.ToString());
+        }
+
+        public Handshake AddParameter(string key, string value) {
+            _params.Add(new MultipartFormDataSection(key, value));
+            return this;
+        }
+
+        public void Shake(Action<UnityWebRequest> complete = null) {
+            var handshake = UnityWebRequest.Post(_webReference + _webController, _params);
+            var request = handshake.SendWebRequest();
+            if (complete == null) return;
+            request.completed += (action) => {
+                complete.Invoke(handshake);
+            };
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/App/Data Management/Handshakes/HandshakeProtocol.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.App.Data_Management {
+    public enum HandshakeProtocol {
+        Request,
+        Response
+    }
+}
