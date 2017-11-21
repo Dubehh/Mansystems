afec52bc0dbd5d44847f03e6fc64822775b8419b
dubehh <eelcoeikelboom@hotmail.com>
2017-11-21 17:29:20 +0100

    formatting

--- Assets/Scripts/App/Data Management/Handshakes/Handshake.cs
+++ Assets/Scripts/App/Data Management/Handshakes/Handshake.cs
@@ -42,7 +42,7 @@ namespace Assets.Scripts.App.Data_Management {
         /// </summary>
         /// <param name="complete">Callback that fires upon completion of the request</param>
         public void Shake(Action<UnityWebRequest> complete = null) {
-            var handshake = UnityWebRequest.Post(_webReference + _webController, _params);
+            var handshake = UnityWebRequest.Post(_webReference + _webController+".php", _params);
             var request = handshake.SendWebRequest();
             if (complete == null) return;
             request.completed += (action) => {
