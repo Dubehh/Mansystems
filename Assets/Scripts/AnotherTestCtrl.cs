42a930be82e0e90c8c2f1c042c8a0986826fbabf
dubehh <eelcoeikelboom@hotmail.com>
2017-11-24 11:39:03 +0100

    test code

--- Assets/Scripts/AnotherTestCtrl.cs
+++ Assets/Scripts/AnotherTestCtrl.cs
@@ -6,30 +6,14 @@ using UnityEngine;
 
 public class AnotherTestCtrl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public void ClickMe() {
         Debug.Log("Starting test...");
-        //StartCoroutine(Test());
-        var a = new Handshake(HandshakeProtocol.Response).AddParameter("responseHandler", "millionaire");
-        a.SetErrorHandler(()=> {
-            Debug.Log("Error");
-        });
-        a.Shake((response) => {
-            var text = response.downloadHandler.text;
-            var obj = new JSONObject(text);
-            Debug.Log(obj["eelco"].str);
-        });
-
-	}
+        StartCoroutine(Test());
+    }
 
     private IEnumerator Test() {
         Debug.Log("Loading test scene in 5 seconds..");
         yield return new WaitForSeconds(5);
         AppData.Instance().Game.Load("3");
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Assets/Scripts/TestCtrl.cs
+++ Assets/Scripts/TestCtrl.cs
@@ -26,32 +26,7 @@ namespace Assets.Scripts {
         }
 
         protected override void OnLoad() {
-            StartCoroutine(Save());
-        }
-
-        private System.Collections.IEnumerator Save() {
-            yield return new WaitForSeconds(2f);
-            Debug.Log("Testing first handshake...");
-            new Handshake(HandshakeProtocol.Response).AddParameter("moduleReference", "millionaireHandler").Shake((request) => {
-                if (request.isHttpError || request.isNetworkError) {
-                    Debug.Log("Error");
-                } else {
-                    var a = new JSONObject(request.downloadHandler.text);
-                    Debug.Log("Handshake successful, response: " + a["a"]["c"].i);
-                }
-            });
-
-            yield return new WaitForSeconds(2f);
-            Debug.Log("Adding some data to the database..");
-            DataSource.Insert(DataParams.Build("Won", 1).Append("Enemies_Killed", 8));
-            DataSource.Insert(DataParams.Build("Won", 1).Append("Enemies_Killed", 3));
-            DataSource.Insert(DataParams.Build("Won", 0).Append("Enemies_Killed", 4));
-            yield return new WaitForSeconds(2f);
-
-            Debug.Log("Starting to save tracking in 5 seconds..");
-            yield return new WaitForSeconds(5f);
-            Debug.Log("Tracking request has beent sent...");
-            Tracking.RequestSend();
+            
         }
 
         protected override void Update() {}
