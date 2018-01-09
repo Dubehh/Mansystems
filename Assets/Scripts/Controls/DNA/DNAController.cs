b052f22b016f03d218ca70e462ed671d09715528
Dubehh <eelcoeikelboom@hotmail.com>
2018-01-09 12:55:42 +0100

    game-completion feedback

--- Assets/Scripts/Controls/DNA/DNAController.cs
+++ Assets/Scripts/Controls/DNA/DNAController.cs
@@ -4,14 +4,12 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class DNAController : MonoBehaviour {
+
+    [SerializeField] public GameObject CompletionScreen;
     [SerializeField] public GameObject MoreInfo;
-
     [SerializeField] public Text DisplayText;
-
     [SerializeField] public DNAItem[] Items;
-
     [SerializeField] public Manny Manny;
-
     [SerializeField] public ParticleSystem ParticleSystem;
 
     private void Start() {
@@ -22,8 +20,17 @@ public class DNAController : MonoBehaviour {
         }
     }
 
+    private void ValidateCompletion() {
+        if (Items.Any(item => Manny.Attribute.GetAttribute(item.Attribute) < 5))
+            return;
+        CompletionScreen.SetActive(true);
+        
+    }
+
     private void Update() {
-        if (MoreInfo.activeSelf && Input.touchCount > 0) MoreInfo.SetActive(false);
+        if (Input.touchCount <= 0) return;
+        if(MoreInfo.activeSelf) MoreInfo.SetActive(false);
+        if(CompletionScreen.activeSelf) CompletionScreen.SetActive(false);
     }
 
     public void OnClickNavigation() {
@@ -36,14 +43,14 @@ public class DNAController : MonoBehaviour {
     public void OnClickButton() {
         var obj = EventSystem.current.currentSelectedGameObject;
         var slider = obj.GetComponentInParent<Slider>();
-        var item = Items.Where(x => x.Slider == slider).FirstOrDefault();
-        if (item != null) {
-            if (Manny.Attribute.GetAttribute(Attribute.Skillpoints) <= 0 || slider.value >= 5) return;
-            Manny.Attribute.IncrementAttribute(Attribute.Skillpoints, -1);
-            Manny.Attribute.IncrementAttribute(item.Attribute, 1);
-            item.Update();
-            ParticleSystem.Play();
-            DisplayText.text = Manny.Attribute.GetAttribute(Attribute.Skillpoints) + "";
-        }
+        var item = Items.FirstOrDefault(x => x.Slider == slider);
+        if (item == null) return;
+        if (Manny.Attribute.GetAttribute(Attribute.Skillpoints) <= 0 || slider.value >= 5) return;
+        Manny.Attribute.IncrementAttribute(Attribute.Skillpoints, -1);
+        Manny.Attribute.IncrementAttribute(item.Attribute, 1);
+        item.Update();
+        ValidateCompletion();
+        ParticleSystem.Play();
+        DisplayText.text = Manny.Attribute.GetAttribute(Attribute.Skillpoints) + "";
     }
 }
